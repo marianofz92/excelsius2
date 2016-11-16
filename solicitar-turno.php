12ed6a27770127b5c0108e0394f284c1ebcd2c00
Mariano Flores <mariano.floresz92@gmail.com>
2016-11-16 17:27:23 -0300

    avance turnos

--- solicitar-turno.php
+++ solicitar-turno.php
@@ -96,7 +96,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
       <form action="" method="post" class="formulario-resultado" >
         <ul class="horarios">
             <li><p class="hora1"> HORA </p> <p class="estado1"> ESTADO</p> <p class="lugar1">LUGAR</p></li>
-            <li><p class="hora">08:00</p> <p class="estado"> DISPONIBLE</p></li> <p class="lugar1">San Juan 889</p>
+            <li><p class="hora">08:00</p> <p class="estado"> DISPONIBLE</p> <p class="lugar">SAN JUAN 899</p></li>
             <li><p class="hora">08:15</p> <p class="estado-nd">NO DISPONIBLE</p></li>
             <li><p class="hora">08:30</p></li>
             <li><p class="hora">08:45</p></li>
